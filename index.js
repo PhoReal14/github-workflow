a887e61a9a0e3e19df851d1438958beeb9c46459
Garren Pho <32909715+PhoReal14@users.noreply.github.com>
2023-07-27 19:58:03 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,186 @@
+let value ="";
+if(value === false){
+    console.log ("the boolean value flase is falsy")
+} else if (value === null){
+    console.log("the null value is falsy");
+} else if (value === unidentified){
+    console.log("unidentified is falsy")
+} else if (value === o){
+    console.log("The number 0 is falsy(the only falsy number)")
+} else if (value === ""){
+    console.log("The emoty string is falsy(the only falsy string")
+}
+
+
+
+
+const a = 50;
+
+const b = 51;
+
+let sum = a + b;
+
+if (sum > 100){
+    console.log(sum + " 101 us greater than 100")
+} if
+
+const c = 99;
+const d = -2;
+
+ sum = c + d;
+if (sum > 0){
+    console.log(sum + "97 is greater than 0")
+}
+
+ c = 0;
+ d = 101;
+
+ sum = c + d;
+if (sum > 100){
+    console.log(sum + "101 us greater than 100")
+}
+ c = 500;
+ d = -500;
+
+ sum = c + d;
+if (sum = 100){
+    console.log(sum + "0 is equal to 0")
+}
+ c = -1000;
+ d = 0;
+
+ sum = c + d;
+if (sum < 0){
+    console.log(sum + "-1000 is a negative number")
+}
+ c = -5;
+ d = 0;
+
+ sum = c + d;
+if (sum < 0){
+    console.log(sum + "-5 is a negative number")
+}
+
+
+
+
+
+
+
+ c = 5;
+ d = 6;
+
+ sum = c + d;
+if (sum === true){
+    console.log(sum ="true")
+}
+
+ c = 10;
+ d = 11;
+
+ sum = c + d;
+if (sum === true){
+    console.log(sum ="true")
+}
+
+ c = 0;
+ d = 0;
+
+ sum = c + d;
+if (sum === false){
+    console.log(sum ="false")
+}
+
+ c = 1000;
+ d = -1000;
+
+ sum = c + d;
+if (sum === false){
+    console.log(sum ="false")
+}
+
+ c = 6;
+ d = 4;
+
+ sum = c + d;
+if (sum === false){
+    console.log(sum ="false")
+}
+
+ c = 5;
+ d = 5;
+
+ sum = c + d;
+if (sum > 1){
+    console.log(sum ="true")
+}
+
+
+
+
+
+
+
+
+let param1A = "cat"
+let param1B = "cat"
+let param2A = 6
+let param2B = "6"
+
+if (param1A === param1B && param2A === param2B){
+    console.log("true")
+} 
+
+ param1A = "five"
+ param1B = 5
+ param2A = "dog"
+ param2B = "dawg"
+
+if (param1A === param1B && param2A === param2B){
+    console.log("false")
+} 
+
+param1A = "five"
+param1B = 5
+param2A = "dog"
+param2B = "dawg"
+
+if (param1A === param1B && param2A === param2B){
+   console.log("false")
+} 
+
+param1A = 0
+param1B = false
+param2A = "horse"
+param2B = "horse"
+
+if (param1A === param1B && param2A === param2B){
+   console.log("true")
+} 
+
+param1A = "eight"
+param1B = "eight"
+param2A = "ate"
+param2B = "ate"
+
+if (param1A === param1B && param2A === param2B){
+   console.log("true")
+} 
+
+param1A = 11
+param1B = "eleven"
+param2A = "four"
+param2B = "for"
+
+if (param1A === param1B && param2A === param2B){
+   console.log("false")
+} 
+
+param1A = "cake"
+param1B = "cake"
+param2A = "pie"
+param2B = "pie"
+
+if (param1A === param1B && param2A === param2B){
+   console.log("true")
+} 
